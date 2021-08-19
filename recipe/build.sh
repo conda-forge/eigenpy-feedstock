5fd046415e5ba939311de032ae3349eca6007508
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-19 13:57:29 +0200

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,6 @@ cd build
 
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DPYTHON_EXECUTABLE=$PYTHON
 make
 make install
