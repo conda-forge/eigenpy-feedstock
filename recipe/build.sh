e518d6513898cc0f9be7095dbb64fc9e65239392
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-20 17:40:26 +0200

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 mkdir build
 cd build
 
+#ls $SP_DIR/numpy/core/include
+echo $( $PYTHON -c "import numpy; print (numpy.get_include())")
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=$PYTHON
