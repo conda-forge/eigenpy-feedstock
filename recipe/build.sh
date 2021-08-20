6bf01ceafb08211651961797287a4caa17103a0f
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-20 18:52:49 +0200

    fix patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,10 @@ export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
 echo $BUILD_NUMPY_INCLUDE_DIRS
 echo $TARGET_NUMPY_INCLUDE_DIRS
 
-ln -s BUILD_NUMPY_INCLUDE_DIRS TARGET_NUMPY_INCLUDE_DIRS
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+fi
 
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
@@ -18,3 +21,10 @@ cmake ${CMAKE_ARGS} .. \
       -DNUMPY_INCLUDE_DIRS=$TARGET_NUMPY_INCLUDE_DIRS
 make
 make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/eigenpy/eigenpyTargets.cmake
+  rm $PREFIX/lib/cmake/eigenpy/eigenpyTargets.cmake.back
+fi
