887ff831268efca6261997f2ea5fc82f4be6dd42
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-20 18:22:49 +0200

    build: fix cross compilation issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,17 @@ mkdir build
 cd build
 
 #ls $SP_DIR/numpy/core/include
-echo $( $PYTHON -c "import numpy; print (numpy.get_include())")
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+ln -s BUILD_NUMPY_INCLUDE_DIRS TARGET_NUMPY_INCLUDE_DIRS
 
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DPYTHON_EXECUTABLE=$PYTHON
+      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DNUMPY_INCLUDE_DIRS=$TARGET_NUMPY_INCLUDE_DIRS
 make
 make install
